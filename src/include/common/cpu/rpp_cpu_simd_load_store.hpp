a9ce5f27482b4fc9e63a5e966676b82f60d4e733
RooseweltMcW <austin.roosewelt@multicorewareinc.com>
2025-04-03 06:51:44 +0000

    Added : Load F16 support for Tensor_mean, Tensor_stddev, Tensor_sum

--- src/include/common/cpu/rpp_cpu_simd_load_store.hpp
+++ src/include/common/cpu/rpp_cpu_simd_load_store.hpp
@@ -1648,6 +1648,36 @@ inline void rpp_load24_f32pkd3_to_f64pln3_avx(Rpp32f *srcPtr, __m256d *p)
     p[5] = _mm256_cvtps_pd(p128[6]);
 }
 
+inline void rpp_load24_f16pkd3_to_f64pln3_avx(Rpp16f *srcPtr, __m256d *p)
+{
+    __m128 p128[8];
+    __m256 px[8];
+    px[0] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr))));
+    px[1] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr + 3))));
+    px[2] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr + 6))));
+    px[3] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr + 9))));
+    p128[0] = _mm256_castps256_ps128(px[0]);
+    p128[1] = _mm256_castps256_ps128(px[1]);
+    p128[2] = _mm256_castps256_ps128(px[2]);
+    p128[3] = _mm256_castps256_ps128(px[3]);
+    _MM_TRANSPOSE4_PS(p128[0], p128[1], p128[2], p128[3]);
+    px[4] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr + 12))));
+    px[5] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr + 15))));
+    px[6] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr + 18))));
+    px[7] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr + 21))));
+    p128[4] = _mm256_castps256_ps128(px[4]);
+    p128[5] = _mm256_castps256_ps128(px[5]);
+    p128[6] = _mm256_castps256_ps128(px[6]);
+    p128[7] = _mm256_castps256_ps128(px[7]);
+    _MM_TRANSPOSE4_PS(p128[4], p128[5], p128[6], p128[7]);
+    p[0] = _mm256_cvtps_pd(p128[0]);
+    p[1] = _mm256_cvtps_pd(p128[4]);
+    p[2] = _mm256_cvtps_pd(p128[1]);
+    p[3] = _mm256_cvtps_pd(p128[5]);
+    p[4] = _mm256_cvtps_pd(p128[2]);
+    p[5] = _mm256_cvtps_pd(p128[6]);    
+}
+
 inline void rpp_load24_f32pln3_to_f32pln3_avx(Rpp32f *srcPtrR, Rpp32f *srcPtrG, Rpp32f *srcPtrB, __m256 *p)
 {
     p[0] = _mm256_loadu_ps(srcPtrR);
@@ -1714,6 +1744,27 @@ inline void rpp_load24_f32pln3_to_f64pln3_avx(Rpp32f *srcPtrR, Rpp32f *srcPtrG, 
     px[0] = _mm256_loadu_ps(srcPtrR);
     px[1] = _mm256_loadu_ps(srcPtrG);
     px[2] = _mm256_loadu_ps(srcPtrB);
+    px128[0] = _mm256_castps256_ps128(px[0]);
+    px128[1] = _mm256_extractf128_ps(px[0], 1);
+    px128[2] = _mm256_castps256_ps128(px[1]);
+    px128[3] = _mm256_extractf128_ps(px[1], 1);
+    px128[4] = _mm256_castps256_ps128(px[2]);
+    px128[5] = _mm256_extractf128_ps(px[2], 1);
+    p[0] = _mm256_cvtps_pd(px128[0]);
+    p[1] = _mm256_cvtps_pd(px128[1]);
+    p[2] = _mm256_cvtps_pd(px128[2]);
+    p[3] = _mm256_cvtps_pd(px128[3]);
+    p[4] = _mm256_cvtps_pd(px128[4]);
+    p[5] = _mm256_cvtps_pd(px128[5]);
+}
+
+inline void rpp_load24_f16pln3_to_f64pln3_avx(Rpp16f *srcPtrR, Rpp16f *srcPtrG, Rpp16f *srcPtrB, __m256d *p)
+{
+    __m128 px128[6];
+    __m256 px[3];
+    px[0] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtrR))));
+    px[1] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtrG))));
+    px[2] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtrB))));
     px128[0] = _mm256_castps256_ps128(px[0]);
     px128[1] = _mm256_extractf128_ps(px[0], 1);
     px128[2] = _mm256_castps256_ps128(px[1]);
@@ -1801,6 +1852,17 @@ inline void rpp_load8_f32_to_f64_avx(Rpp32f *srcPtr, __m256d *p)
     __m128 px128[2];
     __m256 px;
     px = _mm256_loadu_ps(srcPtr);
+    px128[0] = _mm256_castps256_ps128(px);
+    px128[1] = _mm256_extractf128_ps(px, 1);
+    p[0] = _mm256_cvtps_pd(px128[0]);
+    p[1] = _mm256_cvtps_pd(px128[1]);
+}
+
+inline void rpp_load8_f16_to_f64_avx(Rpp16f *srcPtr, __m256d *p)
+{
+    __m128 px128[2];
+    __m256 px;
+    px = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr))));
     px128[0] = _mm256_castps256_ps128(px);
     px128[1] = _mm256_extractf128_ps(px, 1);
     p[0] = _mm256_cvtps_pd(px128[0]);
