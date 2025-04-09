ae2e8a604e1fbda32cd80e73721e76e671012475
RooseweltMcW <austin.roosewelt@multicorewareinc.com>
2025-04-09 08:20:36 +0000

    Update: F16 load store improvement without 256 conversion in Load operation

--- src/include/common/cpu/rpp_cpu_simd_load_store.hpp
+++ src/include/common/cpu/rpp_cpu_simd_load_store.hpp
@@ -1651,31 +1651,27 @@ inline void rpp_load24_f32pkd3_to_f64pln3_avx(Rpp32f *srcPtr, __m256d *p)
 inline void rpp_load24_f16pkd3_to_f64pln3_avx(Rpp16f *srcPtr, __m256d *p)
 {
     __m128 p128[8];
-    __m256 px[8];
-    px[0] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr))));
-    px[1] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr + 3))));
-    px[2] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr + 6))));
-    px[3] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr + 9))));
-    p128[0] = _mm256_castps256_ps128(px[0]);
-    p128[1] = _mm256_castps256_ps128(px[1]);
-    p128[2] = _mm256_castps256_ps128(px[2]);
-    p128[3] = _mm256_castps256_ps128(px[3]);
+
+    p128[0] = _mm_cvtph_ps(_mm_loadu_si128(reinterpret_cast<const __m128i *>(srcPtr)));
+    p128[1] = _mm_cvtph_ps(_mm_loadu_si128(reinterpret_cast<const __m128i *>(srcPtr + 3))); 
+    p128[2] = _mm_cvtph_ps(_mm_loadu_si128(reinterpret_cast<const __m128i *>(srcPtr + 6))); 
+    p128[3] = _mm_cvtph_ps(_mm_loadu_si128(reinterpret_cast<const __m128i *>(srcPtr + 9))); 
+
     _MM_TRANSPOSE4_PS(p128[0], p128[1], p128[2], p128[3]);
-    px[4] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr + 12))));
-    px[5] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr + 15))));
-    px[6] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr + 18))));
-    px[7] = _mm256_cvtph_ps(_mm_castps_si128(_mm_loadu_ps(reinterpret_cast<Rpp32f *>(srcPtr + 21))));
-    p128[4] = _mm256_castps256_ps128(px[4]);
-    p128[5] = _mm256_castps256_ps128(px[5]);
-    p128[6] = _mm256_castps256_ps128(px[6]);
-    p128[7] = _mm256_castps256_ps128(px[7]);
+
+    p128[4] = _mm_cvtph_ps(_mm_loadu_si128(reinterpret_cast<const __m128i *>(srcPtr + 12)));
+    p128[5] = _mm_cvtph_ps(_mm_loadu_si128(reinterpret_cast<const __m128i *>(srcPtr + 15)));
+    p128[6] = _mm_cvtph_ps(_mm_loadu_si128(reinterpret_cast<const __m128i *>(srcPtr + 18)));
+    p128[7] = _mm_cvtph_ps(_mm_loadu_si128(reinterpret_cast<const __m128i *>(srcPtr + 21)));
+
     _MM_TRANSPOSE4_PS(p128[4], p128[5], p128[6], p128[7]);
+
     p[0] = _mm256_cvtps_pd(p128[0]);
     p[1] = _mm256_cvtps_pd(p128[4]);
     p[2] = _mm256_cvtps_pd(p128[1]);
     p[3] = _mm256_cvtps_pd(p128[5]);
     p[4] = _mm256_cvtps_pd(p128[2]);
-    p[5] = _mm256_cvtps_pd(p128[6]);    
+    p[5] = _mm256_cvtps_pd(p128[6]);   
 }
 
 inline void rpp_load24_f32pln3_to_f32pln3_avx(Rpp32f *srcPtrR, Rpp32f *srcPtrG, Rpp32f *srcPtrB, __m256 *p)
